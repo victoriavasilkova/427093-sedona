6306a1b40be6eb6a6cc42a3df32fe3f7f6177572
victoriavasilkova <victoriavasilkova@yandex.ru>
2018-03-30 21:18:01 +0300

    Добавление JS

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,10 @@ var button = document.querySelector(".button-booking");
 var popup = document.querySelector(".modal");
 var entrancedate = popup.querySelector("[name=entrancedate]");
 
+
+popup.classList.add("modal-show")
+
+
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("modal-show");
