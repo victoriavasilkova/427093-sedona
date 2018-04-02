3b61201878127bc06d2eecc52abb2cddb1fc2d48
victoriavasilkova <victoriavasilkova@yandex.ru>
2018-04-02 19:47:05 +0300

    Финальная версия

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,23 @@
 var button = document.querySelector(".button-booking");
 var popup = document.querySelector(".modal");
 var entrancedate = popup.querySelector("[name=entrancedate]");
+var departuredate = popup.querySelector("[name=departuredate]");
+
+var form = document.querySelector("form");
 
 
-popup.classList.add("modal-show")
 
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("modal-show");
   entrancedate.focus();
+});
+
+
+form.addEventListener("submit", function(evt) {
+  if (!entrancedate.value || !departuredate.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error")
+  }
 });
